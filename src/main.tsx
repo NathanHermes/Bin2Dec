3e7eb40f741699d8b466a0c9befa30728ae25771
Nathan Hermes <hermesnathan8@gmail.com>
2023-12-25 15:14:00 -0300

    feat: update main.tsx file
    - Change import path of files
    - Add route '/'

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
+import { App } from '@/App'
+import '@/index.css'
+import { Home } from '@/page/home'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
 
 const routes = createBrowserRouter([
   {
-    element: <App />
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> }
+    ]
   }
 ])
 
