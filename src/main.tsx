d0fbd5f7ed0272079c3f06a814154c4ac1f3bce7
Nathan Hermes <hermesnathan8@gmail.com>
2023-12-25 13:12:08 -0300

    feat: add routes logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const routes = createBrowserRouter([
+  {
+    element: <App />
+  }
+])
+
+const root = createRoot(
+  document.getElementById('root') as HTMLElement
+)
+
+root.render(
+  <StrictMode>
+    <RouterProvider router={routes} />
+  </StrictMode>
 )
