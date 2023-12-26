1d80f2ec487d7e5673f93d07d4118c8ad3dbfa1f
Nathan Hermes <hermesnathan8@gmail.com>
2023-12-25 21:27:33 -0300

    feat: add page style

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -2,13 +2,13 @@ import { ChangeEvent, useState } from 'react'
 
 export const Home = () => {
   const [error, setError] = useState<string>()
-  const [decimal, setDecimal] = useState<number>()
+  const [decimal, setDecimal] = useState<string>('...')
 
   const handleChange = async (_event: ChangeEvent<HTMLInputElement>) => {
     _event.preventDefault()
     const message: string = _event.target.value
     let hasError: Array<boolean> = []
-    setDecimal(undefined)
+    setDecimal('...')
 
     hasError = await Promise.all(message
       .split('')
@@ -36,16 +36,44 @@ export const Home = () => {
       .map((_number: number, _index: number) => Math.pow(2, (_index)) * _number)
       .reduce((_binary: number, _decimal: number) => _binary + _decimal)
 
-    setDecimal(_decimal)
+    setDecimal(_decimal.toString())
   }
 
   return (
-    <>
-      <label htmlFor="binary">Enter a <span>binary number</span> to convert by <span>decimal number</span>.</label>
-      <input id="binary" type="text" maxLength={8} onChange={event => handleChange(event)} />
-      {error && (<span>{error}</span>)}
-      {decimal && (<span>{decimal}</span>)}
-    </>
+    <main className='w-full h-screen flex flex-col items-center justify-center gap-6 p-4'>
+      <h1 className='text-4xl text-center font-black'><span className='text-violet-500 opacity-40'>000</span>Bin2Dec<span className='opacity-40 text-violet-500'>000</span></h1>
+
+      <div className=' flex-col flex items-center justify-center gap-4'>
+        <p className='text-sm text-center text-zinc-300'>
+          Enter a&nbsp;
+          <a
+            href='https://en.wikipedia.org/wiki/Binary_number'
+            target='_blank'
+            className='bg-violet-500 bg-opacity-20 transition-all hover:underline '>
+            binary number
+          </a>
+          &nbsp;to convert by&nbsp;
+          <a
+            href='https://en.wikipedia.org/wiki/Decimal'
+            target='_blank'
+            className='bg-violet-500 bg-opacity-20 transition-all hover:underline'>
+            decimal number
+          </a>.
+        </p>
+
+        <input
+          type="text"
+          maxLength={8}
+          onChange={event => handleChange(event)}
+          className='bg-transparent border-2 border-opacity-20 border-violet-500 outline-0 p-3 text-center text-sm w-full focus:bg-opacity-20 focus:bg-violet-500 ' />
+        {error && (
+          <span className='text-red-500 text-sm'>OPS! {error}</span>
+        )}
+        {decimal && (
+          <span className='font-black text-violet-100 text-3xl'>{decimal}</span>
+        )}
+      </div>
+    </main>
   )
 
 }
