a7edfd9d0c3f130efaace63e9949a450a84fb93c
Nathan Hermes <hermesnathan8@gmail.com>
2023-12-25 21:27:01 -0300

    feat: add tailwindcss and postcss config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: ['Roboto Mono', 'monospace']
+    },
   },
   plugins: [],
 }
